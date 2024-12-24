9dc3347930c1a5d0ccbb18fb49fc3c4fa94718d5
Yuganshu <yuganshu219@gmail.com>
2024-12-24 21:59:04 +0530

    setup pokemon cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,37 @@
-<script>
+<script lang="ts">
+  import PokemonCard from "$features/card/components/PokemonCard.svelte";
   import Marquee from "$features/search/components/Marquee.svelte";
   import SearchBar from "$features/search/components/SearchBar.svelte";
+  import data from "$lib/db/data.json";
+  import type { Pokemon } from "$lib/types";
+
+  const pokemonsToDisplay = data as Pokemon[];
 </script>
 
 <section class="pt-8 space-y-4">
   <SearchBar />
   <Marquee />
 </section>
+
+<section class="pt-8">
+  <div class="card-grid">
+    {#each pokemonsToDisplay.slice(0, 12) as pokemon (pokemon.id)}
+      <PokemonCard {pokemon} />
+    {/each}
+  </div>
+</section>
+
+<style>
+  .card-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(8rem, 8.001rem));
+    place-content: center;
+    gap: 1rem;
+  }
+
+  @media (min-width: 640px) {
+    .card-grid {
+      grid-template-columns: repeat(auto-fit, minmax(280px, 281px));
+    }
+  }
+</style>
